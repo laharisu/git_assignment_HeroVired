2af257bff579e2c8671c8794009b84104434eb5e
laharisu <128566918+laharisu@users.noreply.github.com>
2023-11-20 01:36:52 +0530

    Update calculatorpluss.py

--- calculatorpluss.py
+++ calculatorpluss.py
@@ -1,4 +1,22 @@
+import math
 
+class Calculator:
+
+def add(self, a, b):
+
+return a + b
+
+def subtract(self, a, b):
+
+return a - b
+
+def multiply(self, a, b):
+
+return a * b
+
+def divide(self, a, b):
+
+return a / b
 
 # TODO: Implement the following function to calculate the square root of a number.
 
